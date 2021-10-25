f8137539b0bf530b7d256bb979194bc253ce89bc
Hailey Thomas <haileyraebeauty@gmail.com>
2021-10-25 14:05:16 -0700

    get button working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,60 @@
 // VARIABLES
-var formEl = document.querySelector(".city-form");
-var inputEl = document.querySelector("#city");
-//example vars
-var search = "harry potter";
-var search2;
+var formEl = document.querySelector("#hero-form");
+var inputEl = document.querySelector("#title");
+var search;
+//movie data
+var movieTitle = [];
+var movieDate = [];
+var moviePoster = [];
+//book data
+var bookTitle = [];
+var bookDate = [];
+var bookDesc = [];
+var bookThumbnail = [];
+
+//we use arrays for data because there are 5 items we want to store specific data for
+//ex. 5 book titles
+
 // FORM SUBMIT HANDLER
 var formSubmitHandler = function (event) {
     // prevent page from reloading
     event.preventDefault();
     // get value from input element
-    sign = inputEl.value.trim();
+    search = inputEl.value.trim();
     // run get city function
-    getCity();
+    getMovie();
+    getBook();
 };
 
-// GET AFFIRMATION
-var getAffirmation = function () {
-    var url2 = "http://www.omdbapi.com/?s=" + search + "&apikey=5bdbab43&";
+// GET MOVIE
+var getMovie = function () {
+    var movieUrl = "http://www.omdbapi.com/?s=" + search + "&apikey=5bdbab43&";
 
     // make fetch request
-    fetch(url2).then(function (response) {
+    fetch(movieUrl).then(function (response) {
         // if request successful
         if (response.ok) {
             // get data from response
             console.log(response);
             response.json().then(function (data) {
                 console.log(data);
-                // console log affirmation
-                console.log(data.affirmation);
-                //declare element for affirmation and add it to page
+                //log data for 5 movies using for loop
+                for (i = 0; i < 5; i++) {
+                    //movie title
+                    movieTitle[i] = data.Search[i].Title
+                    console.log("Title: " + movieTitle[i]);
+
+                    //year released
+                    movieDate[i] = data.Search[i].Year
+                    console.log("Year: " + movieDate[i]);
+
+                    //image of the poster
+                    moviePoster[i] = data.Search[i].Poster
+                    console.log("Poster: " + moviePoster[i]);
+                }
+                //side note for grabbing movie data
+                //REMEMBER TO USE CAPITALS
+                //ex. data.Search[0].Title
             });
         } else {
             alert("Error getting api response");
@@ -36,9 +62,9 @@ var getAffirmation = function () {
     });
 };
 
-// GET HOROSCOPE
+// GET BOOK
 
-var getHoroscope = function () {
+var getBook = function () {
     var url = "https://www.googleapis.com/books/v1/volumes?q=" + search + "&key=AIzaSyDHFrhaSZyG8xtzgAEnpoZ8Rh5zLZ-D0RU";
 
     // make fetch request
@@ -49,8 +75,28 @@ var getHoroscope = function () {
             console.log(response);
             response.json().then(function (data) {
                 console.log(data);
-                // console log
-                console.log();
+                //log data for 5 books using for loop
+                for (i = 0; i < 5; i++) {
+                    //booktitle
+                    bookTitle[i] = data.items[i].volumeInfo.title
+                    console.log("Title: " + bookTitle[i]);
+
+                    //date released
+                    bookDate[i] = data.items[i].volumeInfo.publishedDate
+                    console.log("Released: " + bookDate[i]);
+                    //some descriptions are empty and we don't want to have desc: undefined. so use if statement
+                    if (data.items[i].volumeInfo.description) {
+                        bookDesc[i] = data.items[i].volumeInfo.description
+                        console.log("Description: " + bookDesc[i]);
+                    }
+                    else {
+                        console.log("Description: No description available.");
+                    }
+                    //post the image of the content
+                    bookThumbnail[i] = data.items[i].volumeInfo.imageLinks.thumbnail
+                    console.log("Thumbnail: " + bookThumbnail[i]);
+                }
+
                 //declare element add it to page
             });
         } else {
@@ -59,8 +105,8 @@ var getHoroscope = function () {
     });
 }
 
-// CALL FUNCTIONS
-getAffirmation();
-getHoroscope();
+
 // SUBMIT CITY EVENT LISTENER
 formEl.addEventListener("submit", formSubmitHandler);
+
+//log titles, images, descriptions
